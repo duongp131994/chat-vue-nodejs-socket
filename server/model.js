0acfe05b9d66dfca25be617e8ca0923bd033e721
duongp131994 <duongp131994@gmail.com>
2022-11-26 17:11:26 +0700

    fix model

--- server/model.js
+++ server/model.js
@@ -23,7 +23,7 @@ class ModelContent extends Model {
     // });
 
     createTable() {
-        var sql = `CREATE TABLE chat_content (
+        var sql = `CREATE TABLE IF NOT EXISTS chat_content (
             id INT(11) AUTO_INCREMENT PRIMARY KEY,
             content TEXT, 
             send_date datetime,
@@ -91,19 +91,13 @@ class ModelContent extends Model {
     }
 }
 
-
 class ModelUser extends Model {
-    // con.query(sql, function (err, result) {
-    //     if (err) throw err;
-    //     console.log("Result: " + result);
-    // });
-
     createTable() {
-        var sql = `CREATE TABLE chat_user (
+        var sql = `CREATE TABLE IF NOT EXISTS chat_user (
             id INT(11) AUTO_INCREMENT PRIMARY KEY,
             name varchar(255),
             log_out_date datetime,
-            params INT(11)
+            params TEXT
         )`;
         this.db.query(sql, function (err, result) {
             if (err) throw err;
@@ -116,7 +110,7 @@ class ModelUser extends Model {
         let log_out_date = Date.now();
         let params = data['params'] || JSON.stringify({});
         let b={name:name,log_out_date:log_out_date, params:params};
-        this.db.query('insert into chat_content SET ?', b , function(err, result) {
+        this.db.query('insert into chat_user SET ?', b , function(err, result) {
             if (err) throw err;
 
             console.log("1 record inserted");
@@ -125,42 +119,97 @@ class ModelUser extends Model {
         });
     }
 
-    selectAllInConversion({conversionId}) {
-        let conversion_id = conversionId || '';
-        let sql = 'SELECT * FROM chat_content WHERE conversion_id = ' + mysql.escape(conversion_id);
+    updateInto({data}) {
+        let sql = 'UPDATE chat_user SET '
+        let b=[]
+
+        if (typeof data['name'] !== 'undefined') {
+            sql += 'name=?'
+            b.push(data['name'])
+        }
+        if (typeof data['log_out_date'] !== 'undefined') {
+            sql += ',log_out_date=?'
+            b.push(data['log_out_date'])
+        }
+        if (typeof data['params'] !== 'undefined') {
+            sql += ',params=?'
+            b.push(data['params'])
+        }
+        b.push(data['id'])
+
+        this.db.query(sql + ' WHERE id = ?', b , function(err, result) {
+            if (err) throw err;
+
+            console.log("1 record inserted");
+
+            return result;
+        });
+    }
+
+    selectUser({userId}) {
+        let sql = 'SELECT * FROM chat_user WHERE id = ' + mysql.escape(userId || '');
         this.db.query(sql , function(err, result) {
             if (err) throw err;
 
             return result;
         });
     }
+}
+class ModelConversion extends Model {
+    createTable() {
+        var sql = `CREATE TABLE IF NOT EXISTS chat_conversion (
+            id INT(11) AUTO_INCREMENT PRIMARY KEY,
+            name varchar(255),
+            users TEXT
+        )`;
+        this.db.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+    }
 
-    selectAllInConversionLimit({conversionId, limit}) {
-        let conversion_id = conversionId || '';
-        let sql = 'SELECT * FROM chat_content WHERE conversion_id = ' + mysql.escape(conversion_id) + ' order by send_date DESC LIMIT ' + mysql.escape(limit);
+    insertInto({data}) {
+        let name = data['name'] || '';
+        let users = data['users'] || JSON.stringify({});
+        let b={name:name, users:users};
+        this.db.query('insert into chat_conversion SET ?', b , function(err, result) {
+            if (err) throw err;
+
+            console.log("1 record inserted");
+
+            return result;
+        });
+    }
+
+    updateInto({data}) {
+        let sql = 'UPDATE chat_conversion SET '
+        let b=[]
+
+        if (typeof data['name'] !== 'undefined') {
+            sql += 'name=?'
+            b.push(data['name'])
+        }
+        if (typeof data['users'] !== 'undefined') {
+            sql += ',users=?'
+            b.push(data['users'])
+        }
+        b.push(data['id'])
+
+        this.db.query(sql + ' WHERE id = ?', b , function(err, result) {
+            if (err) throw err;
+
+            console.log("1 record inserted");
+
+            return result;
+        });
+    }
+
+    selectUser({id}) {
+        let sql = 'SELECT * FROM chat_conversion WHERE id = ' + mysql.escape(id || '');
         this.db.query(sql , function(err, result) {
             if (err) throw err;
 
             return result;
-        });
-    }
-
-    selectAllInConversionTime({conversionId, date}) {
-        let conversion_id = conversionId || '';
-        var sql = 'SELECT * FROM chat_content WHERE conversion_id = ? AND send_date > ?';
-        this.db.query(sql, [conversion_id, date], function (err, result) {
-            if (err) throw err;
-
-            return result;
-        });
-    }
-
-    deleteConversion({conversionId}) {
-        let conversion_id = conversionId || '';
-        var sql = 'DELETE FROM chat_content WHERE conversion_id = ?';
-        this.db.query(sql, conversion_id, function (err, result) {
-            if (err) throw err;
-            console.log("Number of records deleted: " + result.affectedRows);
         });
     }
 }
